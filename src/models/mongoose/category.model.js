20d315491f426837cef95f3ca0a5395605eafce0
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-29 09:50:57 -0300

    Virtual configurado para el populate inverso con Asset

--- src/models/mongoose/category.model.js
+++ src/models/mongoose/category.model.js
@@ -16,6 +16,11 @@ const CategorySchema = new Schema(
   { timestamps: true }
 );
 
-// ! FALTA COMPLETAR ACA
+CategorySchema.virtual("assets", {
+  ref: "Asset",
+  localField: "_id",
+  foreignField: "category",
+  justOne: false,
+});
 
 export const CategoryModel = model("Category", CategorySchema);
