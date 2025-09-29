f1a879bc6f1db5bb5b51d2a98fb75aca939e47b7
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-29 10:00:23 -0300

    Relación uno a muchos con user añadida

--- src/models/sequelize/asset.model.js
+++ src/models/sequelize/asset.model.js
@@ -1,4 +1,5 @@
 import { DataTypes } from "sequelize";
+import { UserModel } from "./user.model.js";
 
 export const AssetModel = sequelize.define("Asset", {
   inventory_number: {
@@ -22,3 +23,15 @@ export const AssetModel = sequelize.define("Asset", {
 // * 1:N User → Asset (responsible)
 // * 'assets' (User) y 'responsible' (Asset)
 // ! FALTA COMPLETAR ACA
+
+AssetModel.belongsTo(UserModel, {
+  foreignKey: "user_id",
+  targetKey: "id",
+  as: "User",
+});
+
+UserModel.hasMany(AssetModel, {
+  foreignKey: "user_id",
+  sourceKey: "id",
+  as: "Assets",
+});
