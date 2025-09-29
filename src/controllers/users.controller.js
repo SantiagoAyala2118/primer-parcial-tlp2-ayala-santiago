b75a0d42bdad99f9de40b5ae9c9dec95c7355bb4
Santiagio Ayala <santitomayala07@gmail.com>
2025-09-29 11:24:04 -0300

    Se avanzó en los controladores

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -4,7 +4,8 @@ export const getAllUsers = async (_req, res) => {
   try {
     // TODO: devolver usuarios con profile y sus assets con sus categories (populate) (solo admin)
 
-    const users = await UserModel.find();
+    const users = await UserModel.find().populate("assets");
+
     return res.status(200).json({ data: users });
   } catch (error) {
     console.log(error);
@@ -13,8 +14,19 @@ export const getAllUsers = async (_req, res) => {
 };
 
 export const deleteUser = async (req, res) => {
+  const { id } = req.params;
   try {
     // TODO: eliminación lógica (deletedAt) (solo admin)
+
+    await UserModel.findByIdAndUpdate(
+      id,
+      {
+        $set: { deletedAt: true },
+      },
+      {
+        new: true,
+      }
+    );
     return res.status(204).json({ msg: "Usuario eliminado correctamente" });
   } catch (error) {
     console.log(error);
